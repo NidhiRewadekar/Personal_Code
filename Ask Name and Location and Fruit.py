b6982f9264b69e3e5c2e61f18502758ccb43979d
Nidhi <92314870+NidhiRewadekar@users.noreply.github.com>
2022-12-22 16:45:52 +0000

    Create Ask Name and Location and Fruit.py

---
+++ Ask Name and Location and Fruit.py
@@ -0,0 +1,5 @@
+def ask_name_and_location_and_fruit(name, location, fruit):
+  print(f"Hello {name}")
+  print(f"How do you find {location}? Do you like it?")
+  print(f"Do you like {fruit}?")
+ask_name_and_location_and_fruit("Jane", "New York", "apples")
