9fa25ec1dc6b4d89ab5508e725dc679538cf1f00
Nidhi <92314870+NidhiRewadekar@users.noreply.github.com>
2023-04-06 11:53:22 +0100

    Create ArrayConversion.java

---
+++ ArrayConversion.java
@@ -0,0 +1,19 @@
+public class ArrayConversion {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		double y = x; // converts the int to a double
+		System.out.println(y); // prints the conversion
+
+		double a = 10.3;
+		int b = (int)a; 
+		System.out.println(b); // the number is rounded down
+		
+		Long v1 = (long)10;
+		Float v2 = (float)10.3;
+		System.out.println(v1 + "," + v2);
+
+	}
+
+}
