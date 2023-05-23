89ef5aa4256aaacaecde252b77d29d3ae5431c54
Nidhi <92314870+NidhiRewadekar@users.noreply.github.com>
2023-05-23 16:12:56 +0100

    Update and rename Swap_Value_Without_New_Variable.java to Swap_Value_With_New_Variable.java

--- Swap_Value_With_New_Variable.java
+++ Swap_Value_With_New_Variable.java
@@ -1,15 +1,21 @@
-public class P1_Swap_Value_Without_New_Variable 
+public class P1_Swap_Value_With_New_Variable 
 {
 	public static void Swap()
 	{
+		int temp;
+		
 		int x=10;
 		System.out.println("Value of x:"+x);
 		
 		int y=0;
 		System.out.println("Value of y:"+y);
 		
+		temp=x;
 		x=y;
-		System.out.println("Value of x now:"+x);
+		y=temp;
+		
+		System.out.println("Value of x after swap:"+x);
+		System.out.println("Value of y after swap:"+y);
 	}
 
 	public static void main(String[] args) 
