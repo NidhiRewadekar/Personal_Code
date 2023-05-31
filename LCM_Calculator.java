eac74ee3352015cbf2bc3f82829f9250701af8d8
Nidhi <92314870+NidhiRewadekar@users.noreply.github.com>
2023-05-31 17:07:12 +0100

    Create LCM_Calculator.java

---
+++ LCM_Calculator.java
@@ -0,0 +1,24 @@
+public class P10_LCM_Calculator 
+{
+public static int GCD_Calculator(int num1, int num2)
+{
+   if(num1==0)
+   {
+	 return num2;
+   }
+  
+   return GCD_Calculator(num2%num1, num1);
+}
+
+public static int LCM_Calculator(int num1, int num2)
+{
+   return (num1/GCD_Calculator(num1, num2))	* num2;
+}
+
+public static void main(String[] args) 
+	{
+	    int num1=6;
+	    int num2=3;
+	    System.out.println("The LCM of num1 and num2 is:"+LCM_Calculator(num1, num2));
+	}
+}
