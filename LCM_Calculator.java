4331ccc3772b3aec08ea442c1de43633821a6293
Nidhi <92314870+NidhiRewadekar@users.noreply.github.com>
2023-06-03 20:33:15 +0100

    Update LCM_Calculator.java

--- LCM_Calculator.java
+++ LCM_Calculator.java
@@ -1,18 +1,18 @@
 public class P10_LCM_Calculator 
 {
-public static int GCD_Calculator(int num1, int num2)
+public static int HCF_Calculator(int num1, int num2)
 {
    if(num1==0)
    {
 	 return num2;
    }
   
-   return GCD_Calculator(num2%num1, num1);
+   return HCF_Calculator(num2%num1, num1);
 }
 
 public static int LCM_Calculator(int num1, int num2)
 {
-   return (num1/GCD_Calculator(num1, num2))	* num2;
+   return (num1/HCF_Calculator(num1, num2))* num2;
 }
 
 public static void main(String[] args) 
